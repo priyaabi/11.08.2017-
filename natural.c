1360341891779cae5017d633119e19e2f27c6f0e
priyaabi <30748998+priyaabi@users.noreply.github.com>
2017-08-11 16:46:14 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, s = 0;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    for(i=1; i <= n; ++i)
+    {
+        s += i;
+    }
+
+    printf("S = %d",s);
+
+    return 0;
+}
